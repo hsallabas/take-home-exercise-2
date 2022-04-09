8ddde7f54c2cab4e69499aa53ad38d0abd7b06f0
hsallabas <sallabashalil@gmail.com>
2022-04-09 21:50:17 +0300

    Create header component and change card component folder

--- frontend-angular/src/app/shared/shared.module.ts
+++ frontend-angular/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CardComponent } from "./card/card.component";
+import { CardComponent } from "./components/card/card.component";
 import { MatCardModule } from "@angular/material/card";
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from "@angular/material";
 
 @NgModule({
-  declarations: [CardComponent],
-  exports: [CardComponent],
-  imports: [CommonModule, MatCardModule]
+  declarations: [CardComponent, HeaderComponent],
+  exports: [CardComponent, HeaderComponent],
+  imports: [CommonModule, MatCardModule, MatToolbarModule]
 })
 export class SharedModule {}
